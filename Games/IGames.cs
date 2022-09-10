0fef5c23493b5cc32a98f532a10ce0a74ae1d529
Eshan Chatterjee <eshan2001221@gmail.com>
2022-09-10 17:08:24 +0530

    Network Game

---
+++ Games/IGames.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Sockets;
+
+namespace Game
+{
+    interface IGames
+    {
+        #region Properties
+        // Name of the game
+        string Name { get; }
+
+        // How many players are needed to start
+        int RequiredPlayers { get; }
+        #endregion // Properties
+
+        #region Functions
+        // Adds a player to the game (should be before it starts)
+        bool AddPlayer(TcpClient player);
+
+        // Tells the server to disconnect a player
+        void DisconnectClient(TcpClient client);
+
+        // The main game loop
+        void Run();
+        #endregion // Functions
+    }
+}
