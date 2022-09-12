1fa077f8dc55a487fda2de9e5625ad0ab5b0da9c
Eshan Chatterjee <eshan2001221@gmail.com>
2022-09-12 13:12:51 +0530

    socket and ping done

---
+++ socket-client/client.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class client
+    {
+        public static int MaxReceiveLength = 255;
+        public static int PortNumber = 6000;
+
+
+        // Main method
+        public static void Main(string[] args)
+        {
+            int len;
+            byte[] buffer = new byte[MaxReceiveLength + 1];
+
+            // Create a TCP/IP Socket
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint serv = new IPEndPoint(IPAddress.Loopback, PortNumber);
+
+            // Connect to the server
+            Console.WriteLine("Connecting to the server...");
+            clientSocket.Connect(serv);
+
+            // Get a message (blocks)
+            len = clientSocket.Receive(buffer);
+            Console.Write("Got a message from the server[{0} bytes]:\n{1}",
+                len, Encoding.ASCII.GetString(buffer, 0, len));
+
+            // Cleanup
+            clientSocket.Close();
+        }
+    }
+}
---
+++ socket-server/server.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ports
+{
+    class server
+    {
+        public static int port = 6000;
+        public static bool running = false;
+        public static Socket ServerSocket;
+
+        public static void InteruptHandler(object handler, ConsoleCancelEventArgs args)
+        {
+            Console.WriteLine("Received SIGINT, shutting down");
+
+            running = false;
+            ServerSocket.Shutdown(SocketShutdown.Both);
+            ServerSocket.Close();
+        }
+
+        public static void Main(String[] args)
+        {
+            Socket clientSocket;
+            byte[] msg = Encoding.ASCII.GetBytes("Hello Client");
+
+            IPEndPoint serv = new IPEndPoint(IPAddress.Any, port);
+
+            ServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            ServerSocket.Bind(serv);
+            ServerSocket.Listen(5);
+
+            Console.CancelKeyPress += InteruptHandler;
+
+            running = true;
+            Console.WriteLine("Running Server");
+
+            while (running)
+            {
+                clientSocket = ServerSocket.Accept();
+                Console.WriteLine("Incoming connection from {0}, replying.", clientSocket.RemoteEndPoint);
+
+                // Send a reply (blocks)
+                clientSocket.Send(msg, SocketFlags.None);
+
+                // Close the connection
+                clientSocket.Close();
+            }
+        }
+    }
+}
