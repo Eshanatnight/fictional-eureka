2cdefd4299cf63417bdf5f0afac6f16c49166956
Eshan Chatterjee <eshan2001221@gmail.com>
2022-09-12 13:41:59 +0530

    WIP

---
+++ UDP FTP/NetworkMessage.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace FTP
+{
+    // This is a Simple datastructure that is used in packet queues
+    public class NetworkMessage
+    {
+        public IPEndPoint Sender { get; set; }
+        public Packet Packet { get; set; }
+    }
+}
